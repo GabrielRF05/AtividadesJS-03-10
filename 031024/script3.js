338ff773608a2dcd63ebf9c3f1aec095b222e8a8
GabrielRF05 <gabrieldosreis512@gmail.com>
2024-10-08 13:14:34 -0300

    Add files via upload

--- 031024/script3.js
+++ 031024/script3.js
@@ -1,16 +1,28 @@
-let carro = []
+let carros = []
 let continuar = true
 
     while(continuar){
 
+    let carro = {}
+
     carro.modelo = prompt("Insira o modelo do carro")
     carro.marca = prompt("Insira a marca do carro")
-    carro.ano = parseFloat("Insira o ano do carro")
-    carro.valor = parseFloat("Insira o valor do carro")
+    carro.ano = parseFloat(prompt("Insira o ano do carro"))
+    carro.valor = parseFloat(prompt("Insira o valor do carro"))
 
+    carros.push(carro);
 
-    prompt("Você gostaria de continuar adicionando dados de carros?")
+    continuar = prompt("Você gostaria de continuar adicionando dados de carros?") === 'sim'
 
     
 }
 
+let carroMaisCaro = carros[0];
+
+for (let i = 1; i < carros.length; i++) {
+    if (carros[i].valor > carroMaisCaro.valor) {
+        carroMaisCaro = carros[i];
+    }
+}
+
+console.log(carroMaisCaro)
